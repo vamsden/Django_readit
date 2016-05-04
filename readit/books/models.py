0d6b2d44dbdd853924b998bca0fa33ab1a9a0aba
vanstinger <vandykfernandes@hotmail.com>
2016-05-05 00:43:11 +0530

    Model methods, included save method

--- readit/books/models.py
+++ readit/books/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
 
 
 class Book(models.Model):
+    # New things learnt. verbose_name, help_text and ManyToMany
+    # Relationship
+    # Refer Django Bulk method
     title = models.CharField(max_length=150)
     authors = models.ManyToManyField("Author", related_name='books')
     review = models.TextField(blank=True, null=True)
@@ -11,7 +14,19 @@ class Book(models.Model):
     is_favorite = models.BooleanField(default=False, verbose_name='Favorite?')
 
     def __str__(self):
-        return self.title
+        # string formatting
+        return "{} by {}".format(self.title, self.list_authors())
+
+    def list_authors(self):
+        # refer list comprehension
+        return ", ".join([author.name for author in self.authors.all()])
+
+    def save(self, *args, **kwargs):
+        if (self.review and self.date_reviewed is None):
+            self.date_reviewed = now()
+
+        # super(Child, self).method()
+        super(Book, self).save(*args, **kwargs)
 
 
 class Author(models.Model):
