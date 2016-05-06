6c3c407282f1629425bea2b338b712074d56b8fc
vanstinger <vandykfernandes@hotmail.com>
2016-05-06 18:36:12 +0530

    Query data and use in views, Learnt to use prefetch_related(), humanize, pluralize, date filters in django template

--- readit/books/views.py
+++ readit/books/views.py
@@ -1,10 +1,19 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
 
 
 def list_books(request):
     # return HttpResponse(request.user.username)
-    context = {}
+    """
+    List the books thats have review
+    """
+
+    books = Book.objects.exclude(
+        date_reviewed__isnull=True).prefetch_related('authors')
+
+    context = {
+        'books': books,
+    }
     return render(request, 'books/list.html', context)
