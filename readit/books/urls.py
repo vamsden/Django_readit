1a1b51fd182df294c74d7e897a4df6fc8e974842
vanstinger <vandykfernandes@hotmail.com>
2016-05-07 19:30:01 +0530

    Configured static and applied css to the templates

--- readit/books/urls.py
+++ readit/books/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.list_books, name='home'),
+    url(r'^$', views.list_books, name='books'),
     url(r'^authors/', views.AuthorList.as_view(), name='authors'),
     url(r'^books/(?P<pk>[-\w]+)/$',
         views.BookDetail.as_view(), name='book-detail'),
--- readit/readit/settings.py
+++ readit/readit/settings.py
@@ -105,6 +105,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 try:
     from local_settings import *
 except ImportError:
